f02cbfab0d73a9635ae38c421f288fb192d6b2ed
Aman Kumar Gupta <115359377+amangupta65@users.noreply.github.com>
2025-12-24 15:56:24 +0530

    Update set.py

--- Practice_Day_1/set.py
+++ Practice_Day_1/set.py
@@ -1,4 +1,5 @@
-#1
+#1 Write a program to find the union, intersection, difference and symmertic difference of two sets
+
 A = {1, 2, 3, 4}
 B = {3, 4, 5, 6}
 
@@ -7,7 +8,7 @@ print("Intersection:", A & B)
 print("Difference (A-B):", A - B)
 print("Symmetric Difference:", A ^ B)
 
-#2
+#2 Remove common elements from two sets
 A = {1, 2, 3, 4}
 B = {3, 4, 5, 6}
 
@@ -19,7 +20,7 @@ B = B - common
 print("A:", A)
 print("B:", B)
 
-#3
+#3 Write a program to check whether one set is a subset of another
 
 A = {1, 2}
 B = {1, 2, 3, 4}
@@ -29,7 +30,7 @@ if A.issubset(B):
 else:
     print("A is not a subset of B")
 
-#4
+#4 Iterate over a set and print only elements greater than a given number
 s = {5, 10, 15, 20, 25}
 num = 12
 
@@ -37,10 +38,10 @@ for i in s:
     if i > num:
         print(i)
 
-#5
+#5 Given a list with duplicate values, convert it into a set and back to a list with unique elements
+
 lst = [1, 2, 2, 3, 4, 4, 5]
-
 unique_list = list(set(lst))
-
 print(unique_list)
+
 
