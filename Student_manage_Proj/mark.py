84bbe31a92f3479c181bd694df7f6607db3a3d9a
Aman Kumar Gupta <115359377+amangupta65@users.noreply.github.com>
2025-12-26 13:02:08 +0530

    Update mark.py

--- Student_manage_Proj/mark.py
+++ Student_manage_Proj/mark.py
@@ -1,1 +1,12 @@
 
+marks_data = {}
+
+def add_marks(roll, marks):
+    marks_data[roll] = marks
+
+def calculate_average(roll):
+    m = marks_data.get(roll, [])
+    if m:
+        return sum(m) / len(m)
+    return 0
+
